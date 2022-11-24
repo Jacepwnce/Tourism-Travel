e31d1f02e8c12bafb52279bd78adaa09a7ceedd2
Anton <jacepwncere@yandex.ru>
2022-11-24 23:34:42 +0700

    try bg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,26 @@
 import './Header.scss'
 
+const  backGroundHeader = new URL('../../new-traveler/src/assets/img/Header/bg.png');
 
 
 function Header() {
     return (
         <header className="header">
+            <img src={backGroundHeader} alt="" srcset="" />
             <div className="header__left">
                 <div className="header__left_logo">
                     
                     <span>Travel</span>
+                 
                 </div>
             </div>
             <div className='header__right'>
-                <div className='navigations'>
-                    <nav>
-                        <a href="">1</a>
-                        <a href="">2</a>
-                        <a href="">3</a>
-                        <a href="">4</a>
+                <div className='header__right_navigations'>
+                    <nav className='navigations__items'>
+                        <a href="">О НАС</a>
+                        <a href="">МАРШРУТЫ</a>
+                        <a href="">ПРАЙС</a>
+                        <a href="">КОНТАКТЫ</a>
                     </nav>
                 </div>
             </div>
